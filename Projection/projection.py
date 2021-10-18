2037914c26d68db4d8b509e8ac449fd167a6f835
Joep Boumans <joep_boumans@live.nl>
2021-10-18 17:51:36 +0200

    Create camera projection model
    Testing logic for transforming Obj file to a 2D image. The 3D vectors/points are mapped onto a 2D frame. The location of this frame and the camera position can be changed to display different sides of the obj

---
+++ Projection/projection.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+####
+X,Y,Z = point = np.array([1,1,11]) #Point/vector 
+print("Vector location: ",point)
+#Rotation of the camera in world coords. The camaera has equal rotations to the world center with 1 on each diagonal
+px,py,pz = P = np.array([0,0,0]) #Position of camera in world
+R = np.array([  (1,0,0,px),
+                (0,1,0,py),
+                (0,0,1,pz),
+                (0,0,0,1)])
+print("Rotation array: ",R)
+####
+f = 1; #Focal length
+x = f*X/Z
+y = f*Y/Z
+print("Film coords, x:",x," y:",y)
+# Transform film to pixels
+# Flipped coords since pixel array starts at the top left
+O_y = O_x = 126
+Sx =256
+Sy =256
+u = round(-x*Sx/10+O_x)
+v = round(-y*Sy/10+O_y)
+print("Pixel coords, u:",u," y:",v)
+cols = rows = 256
+pixelarray = np.zeros((rows,cols))
+pixelarray[u,v] = 1
+print(pixelarray)
+print(pixelarray[u,v])
