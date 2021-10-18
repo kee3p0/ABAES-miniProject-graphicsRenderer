d40ce14f543e1c4e1780053b28af2c23c75fe7bf
Joep Boumans <joep_boumans@live.nl>
2021-10-18 19:50:58 +0200

    Generalize code into getPixel function

--- Projection/projection.py
+++ Projection/projection.py
@@ -1,8 +1,7 @@
 import numpy as np
-import scipy.misc as smp
-
+from PIL import Image as img
 ####
-X,Y,Z = point = np.array([1,1,11]) #Point/vector 
+X,Y,Z = point = np.array([5,5,10]) #Point/vector 
 print("Vector location: ",point)
 #Rotation of the camera in world coords. The camaera has equal rotations to the world center with 1 on each diagonal
 px,py,pz = P = np.array([0,0,0]) #Position of camera in world
@@ -12,7 +11,7 @@ R = np.array([  (1,0,0,px),
                 (0,0,0,1)])
 print("Rotation array: ",R)
 ####
-f = 1; #Focal length
+f = 5; #Focal length
 x = f*X/Z
 y = f*Y/Z
 print("Film coords, x:",x," y:",y)
@@ -25,7 +24,25 @@ u = round(-x*Sx/10+O_x)
 v = round(-y*Sy/10+O_y)
 print("Pixel coords, u:",u," y:",v)
 cols = rows = 256
-pixelarray = np.zeros((rows,cols), dtype=np.uint8)
-pixelarray[u,v] = 1
-img = smp.toimage(pixelarray)
-img.show()
+pixelarray = np.zeros((rows,cols,3), dtype=np.uint8)
+pixelarray[u,v] = [255,0,0]
+image = img.fromarray(pixelarray)
+image.show()
+
+def getPixel(array, point):
+    #Rotation of the camera in world coords. The camaera has equal rotations to the world center with 1 on each diagonal
+    X,Y,Z = point
+    px,py,pz = P = np.array([0,0,0]) #Position of camera in world
+    R = np.array([  (1,0,0,px),
+                    (0,1,0,py),
+                    (0,0,1,pz),
+                    (0,0,0,1)])
+    f = 5; #Focal length
+    x = f*X/Z
+    y = f*Y/Z
+    O_y = O_x = 126
+    Sx =256
+    Sy =256
+    u = round(-x*Sx/10+O_x)
+    v = round(-y*Sy/10+O_y)
+    return u,v
