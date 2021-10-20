37044476c5fe0440e17800602e536a410575b82a
Joep Boumans <joep_boumans@live.nl>
2021-10-20 17:50:55 +0200

    Change the projection logic to first calulate the camera offset for world coords and then apply rotation

--- Projection/projection.py
+++ Projection/projection.py
@@ -1,15 +1,16 @@
 import numpy as np
 from PIL import Image as img
 from scipy.spatial.transform import Rotation as Rx
-
+import math
 
 def getPixel(point, camera):
-    theta  = 45/360*2*np.pi
+    thx = 90
+    theta  = thx/360*2*np.pi
     #Rotation of the camera in world coords. The camaera has equal rotations to the world center with 1 on each diagonal
     cx,cy,cz = camera
     X,Y,Z= point
     X,Y,Z,one = Pw = np.array([X,Y,Z,1])
-    px,py,pz = P = np.array([0,0,0]) #Position of camera in world
+    # px,py,pz = P = np.array([0,0,0]) #Position of camera in world
     C = np.array([  (1,0,0,-cx),
                     (0,1,0,-cy),
                     (0,0,1,-cz),
@@ -21,13 +22,14 @@ def getPixel(point, camera):
                     (np.sin(theta),np.cos(theta),0,0),
                     (0,0,1,0),
                     (0,0,0,1)])
-    T = np.matmul(-R,C)
-    X,Y,Z,one= Pc = np.matmul(R,Pw)+T #Calculate point in camera POV, using R as rotation and T as the position of the camera in the world
+    # T = np.matmul(-R,C)
+
+    X,Y,Z,one= Pc = np.matmul(R,np.matmul(Pw,C)) #Calculate point in camera POV, using R as rotation and T as the position of the camera in the world
     ###
     # Calculate the position of x,y in the camera field
     ###
 
-    f = 2; #Focal length
+    f = 1; #Focal length
     F = np.eye(3,M=4)
     F[0,0] = f
     F[1,1] = f
@@ -36,18 +38,20 @@ def getPixel(point, camera):
     X = cp[0,0]
     Y = cp[1,1]
     Z = cp[2,2]
-    x = f*X/(f+Z)
-    y = f*Y/(f+Z)
+    x = f*X/(Z)
+    y = f*Y/(Z)
+    print("x:",x," y:",y)
     O_y = O_x = 126
-    Sx =256
-    Sy =256
-    u = round(-x*Sx/10+O_x)
-    v = round(-y*Sy/10+O_y)
-    return  u,v
+    Sx =256/8
+    Sy =256/8
+    u = -x*Sx+O_x
+    v = -y*Sy+O_y
+    print(u,",",v)
+    return  math.floor(abs(u)),math.floor(abs(v))
 
 
 
-cx,cy,cz = c_point = np.array([1,1,1])
+cx,cy,cz = c_point = np.array([0,0,0])
 cu,cv,cw,c1 = c = np.array([(1,0,0,-cx),
                             (0,1,0,-cy),
                             (0,0,1,-cz),
@@ -55,9 +59,9 @@ cu,cv,cw,c1 = c = np.array([(1,0,0,-cx),
 cols = rows = 256
 pixelarray = np.zeros((rows,cols,3), dtype=np.uint8)
 
-u,v = getPixel(np.array([1,0,6]),c_point)
-print(u,",",v)
-u1,v1 = getPixel(np.array([0,0,6]),c_point)
+u,v = getPixel(np.array([8,0,2]),c_point)
+
+u1,v1 = getPixel(np.array([0,0,2]),c_point)
 
 
 # if u==u1: 
@@ -66,13 +70,14 @@ u1,v1 = getPixel(np.array([0,0,6]),c_point)
 # elif v==v1:
 #     for i in range(abs(u-u1)):
 #         pixelarray[i+u,v] = [0,255,0]
+# pixelarray[126,126] = [0,0,255]
+pixelarray[u1,v1] = [0,255,0]
 
-pixelarray[u1,v1] = [255,0,0]
-pixelarray[u,v] = [255,0,0]
-u,v = getPixel(np.array([1,1,6]),c_point)
-pixelarray[u,v] = [255,0,0]
-u,v = getPixel(np.array([0,1,6]),c_point)
-pixelarray[u,v] = [255,0,0]
+pixelarray[v,u] = [255,0,0]
+# u,v = getPixel(np.array([1,1,6]),c_point)
+# pixelarray[u,v] = [255,0,0]
+# u,v = getPixel(np.array([0,1,6]),c_point)
+# pixelarray[u,v] = [255,0,0]
 # u,v = getPixel(np.array([1,1,7]),c_point)
 # pixelarray[u,v] = [255,0,0]
 
